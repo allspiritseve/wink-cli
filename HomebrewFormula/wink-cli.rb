e28c55a07f1f6d6b32c7f79851acf9c0100be988
Cory Kaufman-Schofield <cory@corykaufman.com>
2017-12-21 16:50:39 -0500

    Gem

---
+++ HomebrewFormula/wink-cli.rb
@@ -0,0 +1,10 @@
+class WinkCLI < Formula
+  desc 'Wink command-line application'
+  homepage 'https://github.com/allspiritseve/wink-cli'
+  url 'https://github.com/allspiritseve/wink-cli.git'
+
+  def install
+    system "gem install wink-cli"
+    bin.install "wink"
+  end
+end
